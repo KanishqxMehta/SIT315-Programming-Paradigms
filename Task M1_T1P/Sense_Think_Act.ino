1a06122d821245e76dd3327f4d1bd389d153cb7a
KanishqxMehta <132186237+KanishqxMehta@users.noreply.github.com>
2024-02-01 22:23:10 +0530

    Update Sense_Think_Act.ino

--- Task M1_T1P/Sense_Think_Act.ino
+++ Task M1_T1P/Sense_Think_Act.ino
@@ -1,28 +1,32 @@
 void setup()
 {
   Serial.begin(9600);
-  pinMode(12, OUTPUT);
-  pinMode(8, INPUT);
   pinMode(11, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(12, OUTPUT);
+  
+    digitalWrite(12, HIGH);
+  
+  attachInterrupt(digitalPinToInterrupt(2), detectMotion, CHANGE);
 }
 
 void loop()
 {
-  if (!digitalRead(8)) {
-    digitalWrite(11, LOW);
-    delay(250);
+}
+
+void detectMotion(){
+    if (digitalRead(2)) {
     digitalWrite(11, HIGH);
     delay(250);
-    digitalWrite(11, LOW);
-    delay(20);
+    digitalWrite(12, LOW);
+    delay(250);
     Serial.println("Motion Detected");
   } else {
-    digitalWrite(12, LOW);
-    delay(250);
     digitalWrite(12, HIGH);
     delay(250);
-    digitalWrite(12, LOW);
-    delay(20);
+    digitalWrite(11, LOW);
+    delay(250);
     Serial.println("Motion Not Detected");
   }
+
 }
